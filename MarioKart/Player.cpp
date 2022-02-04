74c8b24d584cb5d75f8d3f8d0f4a60928980090c
c00271288 <c00271288@itcarlow.ie>
2022-02-04 13:43:30 +0000

    added name to my work

--- MarioKart/Player.cpp
+++ MarioKart/Player.cpp
@@ -1,15 +1,8 @@
-
+// Scott
 #include <iostream>
 #include "Player.h"
 #include "Game.h"
 
-
-/// <summary>
-/// @author Peter Lowe
-/// @version 1.0
-/// @date may 2016
-/// 
-/// </summary>
 
 Player::Player()
 {
